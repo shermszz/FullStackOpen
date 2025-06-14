4ef4186dcbdbf17fd1215564422d3e6c45f0f20f
shermszz <ShermanTan@MacBook-Pro-112.local>
2025-06-15 00:12:58 +0800

    ex1.9

---
+++ Part1/unicafe/ex1.8.jsx
@@ -0,0 +1,92 @@
+import { useState } from 'react'
+
+const Button = ({onClick, text}) => 
+    <button onClick={onClick}>
+        {text}
+    </button>
+
+const Feedback = () => <h1>give feedback</h1>
+
+//Using Statistics as its own component
+const Statistics = ({good, neutral, bad, all}) => {
+  return (
+    <div>
+    <h1>statistics</h1>
+    <Score text='good' value={good} />
+        <Score text='neutral' value={neutral} />
+        <Score text='bad' value={bad} />
+        <Score text='all' value={all}/>
+        <Average good={good} bad={bad} all={all}/>
+        <Positive good={good} all={all} />
+    </div>
+  )
+} 
+
+const Score = ({text, value}) => {
+  return (
+    <p>
+      {text} {value}
+    </p>
+  )
+}
+
+const Average = ({good, bad, all}) => {
+  if (all === 0) {
+    return (
+      <Score text='average' value={0} />
+    )
+  }
+  const avg = (good - bad) / all
+  return (
+    <Score text='average' value={avg} />
+  )
+}
+
+const Positive = ({good, all}) => {
+   if (all === 0) {
+    return (
+      <Score text='positive' value="0%" />
+    )
+  }
+  const percentageOfPositive = (good / all) * 100
+  return (
+    <Score text='positive' value={`${percentageOfPositive}%`} />
+  )
+}
+
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  //Remove the useState for all compared to Ex1.7
+  const all = good + neutral + bad
+
+  const incrementGood = () => {
+    setGood(good + 1)
+    
+  }
+  const incrementNeutral = () => {
+    setNeutral(neutral + 1)
+    
+  }
+  const incrementBad = () => {
+    setBad(bad + 1)
+    
+  }
+
+  return (
+    <div>
+        <Feedback />
+        <Button onClick={incrementGood} text='good'/>
+        <Button onClick={incrementNeutral} text='neutral'/>
+        <Button onClick={incrementBad} text='bad'/>
+
+        <Statistics good={good} neutral={neutral} bad={bad} all={all}/>
+        
+    </div>
+  )
+}
+
+export default App
