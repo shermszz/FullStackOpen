08bafee8525da4b50d2b249d15eea04a112048e7
shermszz <ShermanTan@192.168.1.141>
2025-06-22 13:20:28 +0800

    ex2.6

---
+++ Part2/ex2.6.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import Note from './Note'
+
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('a new note...')
+  const [showAll, setShowAll] = useState(false)
+
+  const notesToShow = showAll 
+    ? notes 
+    : notes.filter(note => note.important)
+
+  const addNote = (event) => {
+    event.preventDefault() //Prevent page reload on form submission
+    console.log('Button clicked', event.target);
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: String(notes.length + 1)
+    }
+    setNotes(notes.concat(noteObject))
+    setNewNote('Enter new note...')
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  )
+}
+
+export default App 
