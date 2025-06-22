97b4aa5f4e4b8044c41a669b2f23a07f0098b309
shermszz <ShermanTan@192.168.1.141>
2025-06-22 15:45:36 +0800

    ex2.9

---
+++ Part2/ex2.9.jsx
@@ -0,0 +1,78 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [searchName, setSearchName] = useState('')
+
+    
+  const personExists = (name) => persons.some(person => person.name === name)
+
+  const addPerson = (event) => { // Takes in an event because we are handling a form submissio
+    event.preventDefault()
+    if (personExists(newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return // Exit the function if the name already exists
+    }
+    const nameObject = {
+      id: persons.length + 1,
+      name: newName,
+      number: newNumber
+    }
+    setPersons(persons.concat(nameObject)) //Creating a new array and adding this new nameObject to it
+    setNewName('') // Resetting the input field after submission
+    setNewNumber('') // Resetting the input field after submission
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (e) => {
+    console.log(e.target.value)
+    setNewNumber(e.target.value)
+  }
+
+  const handleSearchChange = (e) => {
+    console.log(e.target.value)
+    setSearchName(e.target.value.toLowerCase())
+  }
+
+  const findMatch = (person) => person.name.toLowerCase().includes(searchName)
+  
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <div>
+        <p>filter shown with <input value={searchName} onChange={handleSearchChange}/></p>
+      </div>
+      <h2>add a new</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {/* If input string matches any part of a person's name, show that person's details */}
+        {persons.filter(findMatch) // returns the filtered array of persons
+          .map(filteredPerson => {
+            return (
+              <p key={filteredPerson.id}>
+               {filteredPerson.name} {filteredPerson.number}
+              </p>
+            )
+         })}
+    </div>
+  )
+}
+
+export default App
