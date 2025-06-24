f1f36472112592720cd477c2f9d11e259ac6de27
shermszz <ShermanTan@192.168.1.194>
2025-06-24 17:59:15 +0800

    ex2.12

---
+++ Part2/Part2d/ex2.12/App.jsx
@@ -0,0 +1,126 @@
+import React from 'react'
+import axios from 'axios'
+import {useState, useEffect} from 'react'
+
+/*
+const App =() => {
+    const [persons, setPersons] = useState([])
+
+    useEffect(() => {
+        console.log("useEffect processed")
+        axios
+            .post('http://localhost:3001/persons', personObject) //fetching the persons data from an "external" source  
+            .then(response => {
+                console.log("then() method processed")
+                console.log(response.data)
+                setPersons(response.data) // update the state of persons on local memory
+            })
+    }, [])
+
+    console.log("rendered", persons.length, "people")
+
+    return (
+        <ul>
+            {persons.map(person => 
+                <li key={person.id}> 
+                 {person.name} {person.number}
+                </li>
+            )}
+        </ul>
+   
+    )
+    
+
+}
+
+export default App 
+*/
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [searchName, setSearchName] = useState('')
+
+  useEffect(() => {
+    console.log('Runs once?')
+    axios 
+        .get('http://localhost:3001/persons')
+        .then(response => setPersons(response.data))
+  }, []) //this runs only after the first mount
+    
+  const personExists = (name) => persons.some(person => person.name === name)
+
+  const addPerson = (event) => { 
+    event.preventDefault()
+    if (personExists(newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return // Exit the function if the name already exists
+    }
+    const personObject = {
+      //id: persons.length + 1, No need ID anymore, since Post will handle it on its own
+      name: newName,
+      number: newNumber
+    }
+    axios
+        .post('http://localhost:3001/persons', personObject)
+        .then(response => {
+            console.log(response.data)
+            setPersons(persons.concat(response.data))
+            setNewName('')
+            setNewNumber('')
+        })
+        
+    }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (e) => {
+    console.log(e.target.value)
+    setNewNumber(e.target.value)
+  }
+
+  const handleSearchChange = (e) => {
+    console.log(e.target.value)
+    setSearchName(e.target.value.toLowerCase())
+  }
+
+  const findMatch = (person) => person.name.toLowerCase().includes(searchName)
+  
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <div>
+        <p>filter shown with <input value={searchName} onChange={handleSearchChange}/></p>
+      </div>
+      <h2>add a new</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {/* If input string matches any part of a person's name, show that person's details */}
+        {persons.filter(findMatch) // returns the filtered array of persons
+          .map(filteredPerson => {
+            return (
+              <p key={filteredPerson.id}>
+               {filteredPerson.name} {filteredPerson.number}
+              </p>
+            )
+         })}
+    </div>
+  )
+}
+
+export default App
