1fc82d8b547985d40c0ce32e7e910f0642a65dd9
shermszz <ShermanTan@192.168.1.229>
2025-06-25 23:59:47 +0800

    ex2.19

---
+++ Part2/Part2e/ex2.19/src/components/DisplayCountry.jsx
@@ -0,0 +1,21 @@
+const DisplayCountry = ({ foundCountry }) => {
+    if (foundCountry === null) {
+        return null
+    } 
+    return (
+        <div>
+            <h1>{foundCountry.name.common}</h1>
+            <p>Capital {foundCountry.capital}</p>
+            <p>Area {foundCountry.area}</p>
+
+            <h1>Languages</h1>
+            <ul>
+                {Object.values(foundCountry.languages).map(languages => <li>{languages}</li>)}
+            </ul>
+            <img src={foundCountry.flags.png} alt={foundCountry.flags.alt}/>
+           
+         </div>
+    )
+}
+
+export default DisplayCountry
