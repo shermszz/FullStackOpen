64f8c6abc745548d7b862e2659c3fcbf350e3579
shermszz <tshshermanz@gmail.com>
2025-06-26 15:11:14 +0800

    ex3.1

--- Part3/Part3a/ex3.1/index.js
+++ Part3/Part3a/ex3.1/index.js
@@ -26,60 +26,15 @@ let persons = [
     }
   ]
 
+
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+    response.send('<h1>This is the root directory</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id === id)
-
-  if (person) {
-    response.json(persons)
-  } else {
-    response.status(404).end()
-  }
-  
-})
-
-/*
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(note => note.id !== id)
-
-    response.status(204).end()  
-})
-
-const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
-}
-
-app.post('/api/persons', (request, response) => {
-  const body = request.body
-
-  if (!body.name) {
-    return response.status(400).json({ 
-      error: 'name missing' 
-    })
-  }
-
-  const person = {
-    name: body.name,
-    number: body.number || 000,
-    id: generateId(),
-  }
-
-  persons = persons.concat(person)
-
-  response.json(persons)
-}) */
 
 const PORT = 3001
 app.listen(PORT, () => {
