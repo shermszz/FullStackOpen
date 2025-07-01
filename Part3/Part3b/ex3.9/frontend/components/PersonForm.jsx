04f7cdcbbc5797c65e27ffcfede1a74fcb1ec08b
shermszz <tshshermanz@gmail.com>
2025-07-01 16:49:52 +0800

    ex3.9

---
+++ Part3/Part3b/ex3.9/frontend/components/PersonForm.jsx
@@ -0,0 +1,19 @@
+
+const PersonForm = (props) => {
+    return (
+        <div>
+            <form onSubmit={props.addPerson}>
+                <div>
+                    name: <input value={props.newName} onChange={props.handleNameChange} />
+                </div>
+                <div>
+                    number: <input value={props.newNumber} onChange={props.handleNumberChange} />
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+        </div>
+    )
+}  
+export default PersonForm
