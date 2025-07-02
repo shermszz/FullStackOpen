1c5caa8f32e55d6b3bb54957902b712f397c3fbf
shermszz <tshshermanz@gmail.com>
2025-07-02 23:09:55 +0800

    deleted the old ex3.13

--- Part3/Part3c/ex3.13/frontend/components/Filter.jsx
+++
@@ -1,9 +0,0 @@
-const Filter = ({searchName, handleSearchChange}) => {
-    return (
-        <div>
-        <p>filter shown with <input value={searchName} onChange={handleSearchChange} /></p>
-        </div>
-    )
-}
-
-export default Filter
