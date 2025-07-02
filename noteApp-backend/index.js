467f15777173398fecc7d80a8c0fd319872bba46
shermszz <tshshermanz@gmail.com>
2025-07-03 00:14:40 +0800

    Code up till part3c

---
+++ noteApp-backend/index.js
@@ -0,0 +1,113 @@
+require('dotenv').config()
+const express = require('express')
+const Note = require('./models/note')
+
+const app = express()
+
+
+let notes = []
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(requestLogger) //middleware
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
+})
+
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id).then(note => {
+    if (note){
+      response.json(note)
+    }
+    else {
+      response.status(404).end()
+    }
+  })
+  .catch(error => next(error))
+})
+
+app.post('/api/notes', (request, response) => {
+  console.log('Post request got called')
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing',
+    })
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  })
+
+  note.save().then(savedNote => {
+    console.log('note successfully saved in MongoDB')
+    response.json(savedNote)
+  })
+})
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id).then(result => {
+    console.log('Deleting this note: ', result)
+    response.status(204).end()
+  })
+  .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
+
+  Note.findById(request.params.id)
+    .then(note => {
+      if (!note) {
+        return response.status(404).end()
+      }
+
+      note.content = content
+      note.important = important
+
+      return note.save().then((updatedNote) => {
+        response.json(updatedNote)
+      })
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+  
+  if(error.name === 'CastError') {
+    return response.status(400).send({error: 'malformatted id'})
+  }
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
---
+++ noteApp-backend/models/note.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URL
+
+console.log('connecting to url')
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
+//This is different from frontend where we do export default Note for e.g.
+//Node.js uses CommonJS syntax - which uses require() and module.exports
+//if we did require('/models/notes'), we are importing whatever statement we exported,
+//which in this case is mongoose.model('Note', noteSchema)
