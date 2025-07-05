5e1254b0393e743fc4023a921c6e4519e58853a3
shermszz <tshshermanz@gmail.com>
2025-07-05 22:36:14 +0800

    ex4.4

--- Part4/Part4a/ex4.4/tests/ex4.3.test.js
+++ Part4/Part4a/ex4.4/tests/ex4.3.test.js
@@ -1,10 +1,13 @@
-const { test } = require('node:test')
+const { test, describe  } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-  const blogs = []
 
-  const result = listHelper.dummy(blogs)
-  assert.strictEqual(result, 1)
+describe('dummy test', () => {
+  test('dummy returns one', () => {
+    const blogs = []
+
+    const result = listHelper.dummy(blogs)
+    assert.strictEqual(result, 1)
+  })
 })
