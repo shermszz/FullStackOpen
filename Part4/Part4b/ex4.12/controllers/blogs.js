1bcc0404b43b1a9b09744a6c76d240806e43f622
shermszz <tshshermanz@gmail.com>
2025-07-17 22:53:31 +0800

    ex4.12

---
+++ Part4/Part4b/ex4.12/controllers/blogs.js
@@ -0,0 +1,23 @@
+const Blog = require('../models/blog')
+
+const blogsRouter = require('express').Router()
+
+blogsRouter.get('/', async (request, response) => {
+  const listOfBlogs = await Blog.find({})
+  return response.json(listOfBlogs)
+})
+
+//Refactored to use async / await
+blogsRouter.post('/', async (request, response, next) => {
+  try {
+    const blog = new Blog(request.body)
+
+    const savedNote = await blog.save()
+    return response.status(201).json(savedNote)
+  } catch (exception) {
+    next(exception)
+  }
+})
+
+
+module.exports = blogsRouter
