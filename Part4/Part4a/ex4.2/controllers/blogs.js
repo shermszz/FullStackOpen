5e5ff0bb19edbaca3a07b1fa46f2824b6e2a7c9c
shermszz <tshshermanz@gmail.com>
2025-12-16 00:29:12 +0800

    ex4.2

--- Part4/Part4a/ex4.2/controllers/blogs.js
+++ Part4/Part4a/ex4.2/controllers/blogs.js
@@ -2,18 +2,20 @@ const Blog = require('../models/blog')
 
 const blogsRouter = require('express').Router()
 
-blogsRouter.get('/', (request, response) => {
+blogsRouter.get('/', (request, response, next) => {
   Blog.find({}).then((blogs) => {
     response.json(blogs)
   })
+  .catch(error => next(error))
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', (request, response, next) => {
   const blog = new Blog(request.body)
 
   blog.save().then((result) => {
     response.status(201).json(result)
   })
+  .catch(error => next(error))
 })
 
 module.exports = blogsRouter
