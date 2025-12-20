730b6155005a55b0db851737836a8f1b831df350
shermszz <tshshermanz@gmail.com>
2025-12-20 13:33:40 +0800

    ex4.19

---
+++ Part4/Part4c_d/ex4.19/backend/app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const config = require('./utils/config.js')
+const logger = require('./utils/logger.js')
+const middleware = require('./utils/middleware.js')
+const blogsRouter = require('./controllers/blogs.js')
+const usersRouter = require('./controllers/users.js')
+const loginRouter = require('./controllers/login.js')
+
+const app = express()
+
+logger.info('connecting to MONGODB_URI...')
+
+mongoose.connect(config.MONGODB_URI, { family: 4 })
+  .then(() => {
+    logger.info('Successfully connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error('Error connecting to MongoDB', error.message)
+  })
+
+app.use(cors())
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
---
+++ Part4/Part4c_d/ex4.19/backend/utils/middleware.js
@@ -0,0 +1,34 @@
+const logger = require('./logger')
+
+const requestLogger = (request, response, next) => {
+  logger.info('Method:', request.method)
+  logger.info('Path:  ', request.path)
+  logger.info('Body:  ', request.body)
+  logger.info('---')
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, req, res, next) => {
+  console.log(error.message)
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  }
+  else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
+  else if (error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error')) {
+    //Uniqueness error is named MongoServerError and E11000 is the specific error code for Duplicate key
+    return res.status(400).json({ error: 'expected `username` to be unique' }) //Translate the "server error" into a "bad request" error since it is the client's fault
+  }
+  else if (error.name === 'JsonWebTokenError') {
+    //This error occurs when the token is missing or invalid when verifying
+    return res.status(401).json({ error: 'Invalid token' })
+  }
+  next(error) //Otherwise, pass to the default error handler middleware from the Express library
+}
+
+module.exports = { requestLogger, unknownEndpoint, errorHandler }
