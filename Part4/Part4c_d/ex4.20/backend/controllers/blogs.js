d500a642f5be2034755f2150ab8d9975c6bd59d5
shermszz <tshshermanz@gmail.com>
2025-12-21 15:55:18 +0800

    ex4.20

---
+++ Part4/Part4c_d/ex4.20/backend/controllers/blogs.js
@@ -0,0 +1,67 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 }) //So that the note is revealed (only the username and name), rather then just the noteID
+  response.json(blogs)
+})
+
+blogsRouter.post('/', async (request, response) => {
+  const body = request.body
+  const decodedToken = jwt.verify(request.token, process.env.SECRET) //Could raise a JsonWebTokenError, handled by middleware
+  const userId = decodedToken.id //Id of the user who is making the newBlog
+  if (!userId) {
+    //This is a safeguard check because the id field is not immutable in the token and could be changed next time
+    //This just ensures we literally have the id field in place if we ever refactor our code from id to user_id for example
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(userId) //The actual user object with decodedToken.id
+
+  if (!user) return response.status(401).json({ error: 'UserId missing or not valid' })
+
+  const newBlog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user._id //Assign the newBlog to the this particular user
+  })
+
+  const addedBlog = await newBlog.save() //Saving the newBlog into the Blog Table
+
+  user.blogs = user.blogs.concat(addedBlog._id) //At the same time, we update the user and add this newBlog into this user's blogs array
+  await user.save() //Ensure the changes are saved inside the User database
+
+  response.status(201).json(addedBlog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  //Need to search for the blog first with said :id
+  //Then, we delete it from the database
+  const blogToDelete = await Blog.findByIdAndDelete(request.params.id)
+  if (!blogToDelete) {
+    return response.status(404).end() //The blog we want to delete is not found in the database
+  }
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  //Serach for the blog that we want to update
+  //If it exists, then update it to what the request body is
+  let blogToUpdate = await Blog.findById(request.params.id)
+  if (!blogToUpdate) return response.status(404).end()
+
+  const { title, author, url, likes } = request.body
+  blogToUpdate.title = title
+  blogToUpdate.author = author
+  blogToUpdate.url = url
+  blogToUpdate.likes = likes
+
+  const updatingBlog = await blogToUpdate.save()
+  response.json(updatingBlog)
+})
+
+module.exports = blogsRouter
+
---
+++ Part4/Part4c_d/ex4.20/backend/controllers/users.js
@@ -0,0 +1,36 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.get('/', async (request, response) => {
+  const allUsers = await User.find({}).populate('blogs')
+  response.json(allUsers)
+})
+
+usersRouter.post('/', async (request, response) => {
+  //Creating a new user and store this new user appropriately in the database
+  const { username, name, password } = request.body
+
+  //Ex4.16: Need to manually check the password here, since the mongoose Schema only records the hashed version
+  if (!password) return response.status(400).json({ error: 'password must be given' })
+  else if (password.length < 3) {
+    return response.status(400).json({ error: 'password must be at least 3 characters long' })
+  }
+
+  //First, we hash this users password
+  const passwordHash = await bcrypt.hash(password, 10) //Use the standard 10 saltrounds
+
+  //Then, we create a new user for the mongoose database
+  const newUser = new User({
+    username,
+    name,
+    passwordHash
+  })
+
+  const savedUser = await newUser.save()
+  response.status(201).json(savedUser)
+
+})
+
+
+module.exports = usersRouter
